5ca62794f4eda6a8e87ef03f993b2b57b6963bea
olafimihan abideen <olafimihana@gmail.com>
2023-02-18 13:44:38 +0100

    updating About.jsx

--- src/components/Footer/About.jsx
+++ src/components/Footer/About.jsx
@@ -1,14 +1,15 @@
+import {Link} from 'react-router-dom'
 import classes from './About.module.css'
 
 const About = () => {
     return (
         <div className={classes.col}>
             <h4>About</h4>
-            <a href="#">About us</a>
-            <a href="#">Delivery Information</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms & Conditions</a>
-            <a href="#">Contact us</a>
+            <Link to='home'>About us</Link>
+            <Link to='home'>Delivery Information</Link>
+            <Link to='home'>Privacy Policy</Link>
+            <Link to='home'>Terms & Conditions</Link>
+            <Link to='home'>Contact us</Link>
         </div>
     )
 }
